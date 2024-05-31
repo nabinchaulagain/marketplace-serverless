10625d3cef1b41f6e9bbf9072b5bf117677e3126
nabinchaulagain <nabinchaulagain@lftechnology.com>
2024-05-31 16:11:06 +0545

    Route event via SQS to lambda rather than having the lambda recieve the event directly.

--- services/orders/src/handlers/createOrder.ts
+++ services/orders/src/handlers/createOrder.ts
@@ -1,34 +1,45 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient, PutCommand } from '@aws-sdk/lib-dynamodb';
 
+import { type SQSEvent, type SQSRecord } from 'aws-lambda';
+import pino from 'pino';
+
 import { v4 as uuidv4 } from 'uuid';
 
 interface InputEventSchema {
-    detail: {
-        products: [{ id: string; userId: string; price: number; name: string }];
-        buyerId: string;
-    };
+    products: [{ id: string; userId: string; price: number; name: string }];
+    buyerId: string;
 }
+
+const logger = pino();
 
 const client = new DynamoDBClient();
 const docClient = DynamoDBDocumentClient.from(client);
 
-async function createOrder(event: InputEventSchema): Promise<any> {
-    const { products, buyerId } = event.detail;
-    const document = {
-        id: uuidv4(),
-        buyerId,
-        creationDate: Date.now(),
-        products,
-    };
-    const putCommand = new PutCommand({
-        TableName: process.env.ORDERS_TABLE_NAME,
-        Item: document,
-    });
+async function createOrder(event: SQSEvent): Promise<void> {
+    const orders: InputEventSchema[] = event.Records.map(
+        (record: SQSRecord) => JSON.parse(record.body).detail,
+    );
 
-    await docClient.send(putCommand);
+    for (const order of orders) {
+        const { buyerId, products } = order;
+        const document = {
+            id: uuidv4(),
+            buyerId,
+            creationDate: Date.now(),
+            products,
+        };
+        const putCommand = new PutCommand({
+            TableName: process.env.ORDERS_TABLE_NAME,
+            Item: document,
+        });
 
-    return document;
+        logger.info(
+            `Order created for ${JSON.stringify(products)} by ${buyerId}`,
+        );
+
+        await docClient.send(putCommand);
+    }
 }
 
 export const handler = createOrder;
